83a17e23962d6b5ddc01549c54fd6da9cd5bef6d
zasekle <quickshiftx@gmail.com>
2025-10-06 14:56:37 -0400

    Made sand fall problem nicer

--- codinggame_sand_fall/project.js
+++ codinggame_sand_fall/project.js
@@ -1,61 +1,53 @@
-function isCapital(char) {
-   return /[A-Z]/.test(char); 
-}
+const isCapital = (c) => /^[A-Z]$/.test(c);
 
-var inputs = readline().split(' ');
+let inputs = readline().split(' ');
 const W = parseInt(inputs[0]);
 const H = parseInt(inputs[1]);
 const N = parseInt(readline());
+
+let inBounds = (r,c) => r >= 0 && r < H && c >=0 && c < W;
+let empty = (r,c) => inBounds(r,c) && box[r][c] === '.';
+let emptyBelow = (r, c) => empty(r + 1, c);
+
 const box = Array.from({length: H}, () => Array(W).fill('.'));
 for (let i = 0; i < N; i++) {
-    var inputs = readline().split(' ');
+    let inputs = readline().split(' ');
     const id = inputs[0];
+
+    // Need to start row at -1 so that when the sand piles to the top it will
+    // properly check 'below' it which is index 0.
     let col = parseInt(inputs[1]);
     let row = -1;
-    let dropping = true;
 
-    function checkLeft() {
-        if (col > 0 && box[row + 1][col - 1] === '.') { //check left
+    while (true) {
+        // Check if the sand is at the bottom of the box.
+        if (row === H - 1) break;
+
+        // Check if spot below the sand is empty.
+        if (emptyBelow(row, col)) {
             row++;
-            col--;
-            return true;
+            continue;
         }
-        return false;
-    }
 
-    function checkRight() {
-        if (col < W-1 && box[row + 1][col + 1] === '.') { //check right
-            row++;
-            col++;
-            return true;
-        }
-        return false;
-    }
+        let order = isCapital(id) ? [-1, +1] : [+1, -1];
 
-    while (dropping) {
-        if (row === H-1) { // At bottom
-            dropping = false;
-        } else if (box[row + 1][col] === '.') { // Empty Space Below
-            row++;
-        } else {
-            let moved = false;
-            if (isCapital(id)) { // capital letter
-                moved = checkLeft();
-                if (!moved) moved = checkRight();
-            } else {
-                moved = checkRight();
-                if (!moved) moved = checkLeft();
-            }
-
-            if (!moved) {
-                dropping = false;
+        // Check if it can move down-left or down-right.
+        let moved = false;
+        for (const dc of order) {
+            const new_col = col + dc;
+            if (empty(row + 1, new_col)) {
+                row++;
+                col = new_col;
+                moved = true;
+                break;
             }
         }
+
+        // No where for the sand to fall. Leave it stationary.
+        if (!moved) break;
     }
 
-    if (row > -1) {
-        box[row][col] = id;
-    }
+    if (row > -1) box[row][col] = id;
 }
 
 for (const row of box) {
