29995cd18f48a551c7aab648071cbd5645e571c4
zasekle <quickshiftx@gmail.com>
2025-10-06 17:01:12 -0400

    Cleaned up peaks and valleys code

--- codingame_peaks_and_valleys_in_2d_grid/project.js
+++ codingame_peaks_and_valleys_in_2d_grid/project.js
@@ -1,65 +1,37 @@
 const h = parseInt(readline());
-const grid = []
-for (let i = 0; i < h; i++) {
-    grid.push(readline().split(' ').map(Number));
-}
-
-function buildOutput(str, i, j) {
-    str = str === "NONE" ? "" : str + ", ";
-    return str + "(" + j.toString() + ", " + i.toString() + ")";
-}
-
-const Location = Object.freeze({
-    NOT_SET: 0,
-    BELOW: 1,
-    ABOVE: 2,
-    INVALID: 3,
-});
-
-let above = true;
+const grid = Array.from({ length: h }, () => readline().split(' ').map(Number));
 const w = grid[0].length;
-const dir = [[-1, -1], [0, -1], [+1, -1], [+1, 0], [+1, +1], [0, +1], [-1, +1], [-1, 0]]
+const DIRS = [[-1, -1], [0, -1], [+1, -1], [+1, 0], [+1, +1], [0, +1], [-1, +1], [-1, 0]]
 
 const inBounds = (x, y) => x >= 0 && x < w && y >=0 && y < h;
 
-let peaks = "NONE";
-let valleys = "NONE";
+const peaks = [];
+const valleys = [];
 
 for (let i = 0; i < h; i++) {
     for (let j = 0; j < w; j++) {
-        let loc = Location.NOT_SET;
+
+        let allLower = true;
+        let allHigher = true;
         let cur_pos = grid[i][j];
-        for (const pos of dir) {
-            const x = pos[0] + j;
-            const y = pos[1] + i;
-            if (inBounds(x, y)) {
-                const new_pos = grid[y][x];
-                if(loc == Location.NOT_SET) {
-                    if (cur_pos > new_pos) {
-                        loc = Location.ABOVE;
-                    } else if (cur_pos < new_pos) {
-                        loc = Location.BELOW;
-                    } else {
-                        loc = Location.INVALID;
-                        break;
-                    }
-                } else if (
-                    (cur_pos <= new_pos || loc !== Location.ABOVE) && 
-                    (cur_pos >= new_pos || loc !== Location.BELOW)
-                ) { // The new position is not in line with the old position.
-                    loc = Location.INVALID;
-                    break;
-                }
-            }
+        for (const [dx, dy] of DIRS) {
+            const x = dx + j;
+            const y = dy + i;
+            if (!inBounds(x, y)) continue;
+
+            const new_pos = grid[y][x];
+            if (new_pos === cur_pos) { allLower = false; allHigher = false; }
+            if (new_pos >= cur_pos) allHigher = false;
+            if (new_pos <= cur_pos) allLower = false;
+
+            if (!allLower && !allHigher) break;
         }
 
-        if (loc == Location.ABOVE) {
-            peaks = buildOutput(peaks, i, j);
-        } else if (loc == Location.BELOW) {
-            valleys = buildOutput(valleys, i, j);
-        }
+        if (allHigher) peaks.push([j, i]);
+        if (allLower) valleys.push([j, i]);
     }
 } 
 
-console.log(peaks);
-console.log(valleys);
+const fmt = (arr) => arr.length ? arr.map(([x, y]) => `(${x}, ${y})`).join(', ') : 'NONE';
+console.log(fmt(peaks));
+console.log(fmt(valleys));
